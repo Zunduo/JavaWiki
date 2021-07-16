46cd2a0a9e81549236d2744a59874407764092f1
zunduo <zzd82073880@gmail.com>
2021-07-16 15:30:33 +0800

    3-3 集成mybatis

--- src/main/java/com/zunduo/wiki/controller/TestController.java
+++ src/main/java/com/zunduo/wiki/controller/TestController.java
@@ -1,13 +1,26 @@
 package com.zunduo.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+    @Value("${test.hello: TEST}$")
+    private String testHello;
+
+    @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World Post" + name;
     }
 }
