83007d37a006b8f97d6d48cc90009aff6a3c34dc
zunduo <zzd82073880@gmail.com>
2021-07-18 00:51:08 +0800

    3-7 封装请求参数与返回参数

--- src/main/java/com/zunduo/wiki/controller/EbookController.java
+++ src/main/java/com/zunduo/wiki/controller/EbookController.java
@@ -1,7 +1,9 @@
 package com.zunduo.wiki.controller;
 
 import com.zunduo.wiki.domain.Ebook;
+import com.zunduo.wiki.req.EbookReq;
 import com.zunduo.wiki.resp.CommonResp;
+import com.zunduo.wiki.resp.EbookResp;
 import com.zunduo.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
