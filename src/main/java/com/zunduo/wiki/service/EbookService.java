e8987dfb051e1deabc150c70f65b72516dbf1547
zunduo <zzd82073880@gmail.com>
2021-07-18 00:10:07 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/zunduo/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.zunduo.wiki.service;
+
+import com.zunduo.wiki.domain.Ebook;
+import com.zunduo.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+}
