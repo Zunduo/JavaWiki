a207135f304820af7b30efa26271f48fc5555391
zunduo <zzd82073880@gmail.com>
2021-07-20 21:18:35 +0800

    5-4 电子书布局调整 一行显示三列电子书

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -7,8 +7,8 @@
         v-model:selectedKeys="selectedKeys1"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 1xxxx</a-menu-item>
-      <a-menu-item key="2">nav 2xxxx</a-menu-item>
+      <a-menu-item key="1">nav 1</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
       <a-menu-item key="3">nav 3</a-menu-item>
     </a-menu>
   </a-layout-header>
