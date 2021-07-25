1b646fa716be020c38d75c456537d7e7d33f07b5
zunduo <zzd82073880@gmail.com>
2021-07-25 18:53:06 +0800

    7-4 分类管理按树形结构展示 增加数组转树形结构方法

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -28,7 +28,7 @@
       <a-table
           :columns="columns"
           :row-key="record => record.id"
-          :data-source="categorys"
+          :data-source="level1"
           :loading="loading"
           :pagination="false"
       >
@@ -111,6 +111,18 @@ export default defineComponent({
       }
     ];
     /**
+     * 一级分类树，children属性就是二级分类
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
+     */
+    const level1 = ref();
+     /**
      * 数据查询
      **/
     const handleQuery = () => {
@@ -122,6 +134,10 @@ export default defineComponent({
         const data = response.data;
         if (data.success){
           categorys.value = data.content;
+          console.log("raw data:", categorys.value);
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys.value,0);
+          console.log("tree data:", level1);
         } else {
           message.error(data.message);
         }
@@ -189,7 +205,8 @@ export default defineComponent({
 
     return {
       param,
-      categorys,
+      // categorys,
+      level1,
       columns,
       loading,
       handleQuery,
