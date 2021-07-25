7b523ff38ebc6da0dbeeccd8e8d496f6b7acc95c
zunduo <zzd82073880@gmail.com>
2021-07-25 13:57:21 +0800

    6-11 集成validation，为列表接口增加参数校验

--- src/main/java/com/zunduo/wiki/req/PageReq.java
+++ src/main/java/com/zunduo/wiki/req/PageReq.java
@@ -1,8 +1,15 @@
 package com.zunduo.wiki.req;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
 public class PageReq {
+    @NotNull(message = "the page could not be null")
     private int page;
 
+    @NotNull(message = "the size could not be null")
+    @Max(value = 1000, message = "the max value could not exceed 1000")
     private int size;
 
     public int getPage() {
