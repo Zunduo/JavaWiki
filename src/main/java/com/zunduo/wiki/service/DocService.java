08a3765ab6bcf632b9672d0470e8fbcea0af9a3f
zunduo <zzd82073880@gmail.com>
2021-07-29 03:12:47 +0800

    8-11 增加读取文档内容功能

--- src/main/java/com/zunduo/wiki/service/DocService.java
+++ src/main/java/com/zunduo/wiki/service/DocService.java
@@ -109,5 +109,8 @@ public class DocService {
         criteria.andIdIn(ids);
         docMapper.deleteByExample(docExample);
     }
-
+    public String findContent(Long id){
+        Content content = contentMapper.selectByPrimaryKey(id);
+        return content.getContent();
+    }
 }
