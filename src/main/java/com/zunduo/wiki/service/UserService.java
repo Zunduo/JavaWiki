6efc25dd0fe728ae526f165da98b12c1769ffbdb
zunduo <zzd82073880@gmail.com>
2021-07-31 18:47:45 +0800

    9-4 增加校验用户名不能重复，增加自定义异常

--- src/main/java/com/zunduo/wiki/service/UserService.java
+++ src/main/java/com/zunduo/wiki/service/UserService.java
@@ -4,6 +4,8 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.zunduo.wiki.domain.User;
 import com.zunduo.wiki.domain.UserExample;
+import com.zunduo.wiki.exception.BusinessException;
+import com.zunduo.wiki.exception.BusinessExceptionCode;
 import com.zunduo.wiki.mapper.UserMapper;
 import com.zunduo.wiki.req.UserQueryReq;
 import com.zunduo.wiki.req.UserSaveReq;
@@ -14,6 +16,7 @@ import com.zunduo.wiki.util.UuidUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -65,8 +68,13 @@ public class UserService {
             User user = CopyUtil.copy(req,User.class);
             if(ObjectUtils.isEmpty(req.getId())){
                 //新增
+                if (ObjectUtils.isEmpty(selectByLoginName(req.getLoginName()))){
                 user.setId(uuidUtils.getId());
                 userMapper.insert(user);
+                } else {
+                    // username already exist
+                    throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+                }
             } else {
                 //更新
                 userMapper.updateByPrimaryKey(user);
@@ -79,6 +87,18 @@ public class UserService {
      */
     public void delete(Long id){
        userMapper.deleteByPrimaryKey(id);
+    }
+
+    public User selectByLoginName(String LoginName) {
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        criteria.andLoginNameEqualTo(LoginName);
+        List<User> userList = userMapper.selectByExample(userExample);
+        if (CollectionUtils.isEmpty(userList)) {
+            return null;
+        } else {
+            return userList.get(0);
+        }
     }
 
 }
