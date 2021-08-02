f89c523339fd520b10c7acf487eae0d6fcecd8c3
zunduo <zzd82073880@gmail.com>
2021-08-02 22:49:26 +0800

    11-6 前端增加数值统计组件，显示统计数值

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -28,7 +28,7 @@
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <div class="welcome" v-show="isShowWelcome">
-        <h1>welcome</h1>
+        <the-welcome></the-welcome>
       </div>
       <a-list v-show="!isShowWelcome" item-layout="inline" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
         <template #renderItem="{ item }">
@@ -67,8 +67,12 @@ import { defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios';
 import { message } from 'ant-design-vue';
 import {Tool} from "@/util/tool";
+import TheWelcome from '@/components/the-welcome.vue';
 export default defineComponent({
   name: 'Home',
+  components: {
+    TheWelcome
+  },
   setup() {
     const ebooks = ref();
     const isShowWelcome =ref(true);
